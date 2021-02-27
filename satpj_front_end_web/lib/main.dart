e700f9324fa2dd2e4b0356b16131a112dbe2bb59
Pablo <pablopulido190@gmail.com>
2021-02-26 22:48:32 -0500

    Commitsito
    aprovechaElBug.rar

--- satpj_front_end_web/lib/main.dart
+++ satpj_front_end_web/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:satpj_front_end_web/src/app.dart';
+//holaaaa
 
 void main(){
    WidgetsFlutterBinding.ensureInitialized();
